d3c39ae837f3d14cad41d37b3dcbcf037163678e
unknown <pavankumarirrinki0123@gmail.com>
2023-11-20 01:15:37 +0530

    graphs

--- frontened/src/Components/Admin_panel/Dashboard.js
+++ frontened/src/Components/Admin_panel/Dashboard.js
@@ -1,9 +1,56 @@
-import React from 'react'
-
+import React,{useState} from 'react';
+import "./Dashboard.css";
+import { AiFillDollarCircle } from "react-icons/ai";
+import { BiSolidShoppingBags } from "react-icons/bi";
+import { FaShoppingBag } from "react-icons/fa";
+import { data,pie_data,options,pie_options } from './Graphs/Graphs';
+import { Chart } from "react-google-charts";
 function Dashboard() {
+
+ 
   return (
     <div>
-     mmmmmmmmmmmmm 
+    <h1 class="admin_dashboard">DashBoard</h1>
+    <div style={{display:"flex",justifyContent:"space-around"}}>
+      <div style={{display:"flex",backgroundColor:"white",width:"250px",padding:"15px",borderRadius:"10px"}}>
+      <AiFillDollarCircle style={{color:"blue",backgroundColor:"skyblue",borderRadius:"50%",marginLeft:"15px",fontSize:"40px",padding:"6px"}}/> 
+       <p style={{fontWeight:"bold",marginLeft:"20px"}}>Total Sales<br />$12,234</p>
+    
+     </div>
+     <div style={{display:"flex",backgroundColor:"white",width:"250px",padding:"15px",borderRadius:"10px"}}>
+      <BiSolidShoppingBags style={{color:"green",backgroundColor:"skyblue",borderRadius:"50%",marginLeft:"15px",fontSize:"40px",padding:"6px"}}/> 
+       <p style={{fontWeight:"bold",marginLeft:"20px"}}>Total Orders<br />130</p>
+    
+     </div>
+     <div style={{display:"flex",backgroundColor:"white",width:"250px",padding:"15px",borderRadius:"10px"}}>
+      <FaShoppingBag style={{color:"yellow",backgroundColor:"skyblue",borderRadius:"50%",marginLeft:"15px",fontSize:"40px",padding:"6px"}}/> 
+       <p style={{fontWeight:"bold",marginLeft:"20px"}}>Total Products<br />70</p>
+    
+     </div>
+    </div>
+    <div style={{display:"flex",marginTop:"30px",justifyContent:"space-around",marginBottom:"30px"}}>
+      <div style={{backgroundColor:"white",width:"500px",marginRight:"20px",textAlign:"center"}}>
+    <h4 data="123">Sales Statistics</h4>
+   
+    <Chart
+     chartType="Bar"
+     width="100%"
+     height="400px"
+     data={data}
+     options={options}
+   />
+    </div>
+    <div style={{backgroundColor:"white",width:"500px",marginRight:"30px",textAlign:"center"}}>
+    <h4>Products Statistics</h4>
+    <Chart
+       chartType="PieChart"
+       data={pie_data}
+       options={pie_options}
+       width={"100%"}
+       height={"400px"}
+     />
+    </div>
+    </div>
     </div>
   )
 }
---
+++ frontened/src/Components/Admin_panel/Graphs/Graphs.js
@@ -0,0 +1,26 @@
+
+
+   export const data = [
+    ["Year", "Sales", "Expenses", "Profit"],
+    ["2014", 1000, 400, 200],
+    ["2015", 1170, 460, 250],
+    ["2016", 660, 1120, 300],
+    ["2017", 1030, 540, 350],
+  ];
+  
+  export const options = {
+   
+  };
+  export  const pie_data = [
+    ["Task", "Hours per Day"],
+    ["Work", 11],
+    ["Eat", 2],
+    ["Commute", 2],
+    ["Watch TV", 2],
+    ["Sleep", 7],
+  ];
+  
+  export const pie_options = {
+    title: "My Daily Activities",
+    is3D: true,
+  };
